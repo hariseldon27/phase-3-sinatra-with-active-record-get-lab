c8d471531807406bbf632f0d84b7b3347341b5ae
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-03-03 15:21:16 -0800

    commit tree 7026e942af98cd0a65648d89e5a3a42b81b8b1ab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
+  
   # add routes
-
+  get '/bakeries' do
+    all_bakeries = Bakery.all
+    all_bakeries.to_json
+  end
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+    # bakery.to_json(include: { baked_goods: { only: [:name, :price] }})
+  end
+  get '/baked_goods/by_price' do
+    all_yummy = BakedGood.all.order(price: :desc)
+    all_yummy.to_json( only: [:name, :price] )
+  end
+  get '/baked_goods/most_expensive' do
+    spendy_yummy = BakedGood.all.order(price: :desc).first
+    spendy_yummy.to_json
+  end
 end
